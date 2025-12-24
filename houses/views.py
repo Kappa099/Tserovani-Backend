02d0a192ea0f3ac377ad12b4510cdf64dc05ed43
V.Kapanadze <vako.kapa@gmail.com>
2025-12-25 02:37:50 +0400

    te3

--- houses/views.py
+++ houses/views.py
@@ -3,6 +3,8 @@ from .models import House
 from .serializers import HouseSerializer
 
 class HouseViewSet(viewsets.ModelViewSet):
+    sdds =dsds
+    
     queryset = House.objects.all()
     serializer_class = HouseSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
